fbcbd0a7dff70a7162ca785ee8a820384565bb7d
abdul haseeb <abdul.haseeb@quantum-computing-services.com>
2024-07-03 14:15:52 +0500

    product must be displyed on the frontend as per their respective status in the backend done

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -25,175 +25,191 @@ use App\Mail\ImageUploaded;
 
 class IndexController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
-        $data = [ 
+        $data = [
             "frontProducts" =>  Product::select('products.image_id', 'products.discripition', 'categories.title')
-            ->join('categories', 'products.category_id', '=', 'categories.category_id')
-            ->where('products.status', 1)
-            ->limit(4)
-            ->get(),
+                ->join('categories', 'products.category_id', '=', 'categories.category_id')
+                ->where('products.status', 1)
+                ->limit(4)
+                ->get(),
             "testimonials" => Testimonials::get(),
         ];
 
         return view('welcome')->with(compact('data'));
     }
 
-    public function contentPage($pageId, $pageTitle) {
+    public function contentPage($pageId, $pageTitle)
+    {
         // Your controller logic here
         $data = Post::where([
             ['post_id', $pageId],
-            ['type',1]
+            ['type', 1]
         ])->first();
-        
-        return view('content-page')->with(compact('data'));
 
+        return view('content-page')->with(compact('data'));
     }
 
-    public function gallery(){
-        
-        $data = Image::where('status',1)->get();
+    public function gallery()
+    {
+
+        $data = Image::where('status', 1)->get();
         return view('gallery')->with(compact('data'));
     }
 
-    public function documents(){
-        
+    public function documents()
+    {
+
         return view('documents');
     }
 
-    public function news(){
-        
+    public function news()
+    {
+
         $data = Post::where([
             ['post_id', 36],
-            ['type',1]
+            ['type', 1]
         ])->first();
 
         return view('news')->with(compact('data'));
     }
-        
-    public function safetyTrainingVideos(){
-    
 
-        $stv_result=SafetyTrainingVideo::get();
+    public function safetyTrainingVideos()
+    {
+
+
+        $stv_result = SafetyTrainingVideo::get();
 
         $stv_result = $stv_result[0];
         if (isset($stv_result['description']) && $stv_result['description'] != '')
             $description = $stv_result['description'];
         else
             $description = '';
-        
+
         if (isset($stv_result['youtube_title']) && $stv_result['youtube_title'] != '')
             $youtube_title = unserialize($stv_result['youtube_title']);
         else
             $youtube_title = '';
-        
+
         if (isset($stv_result['youtube_url']) && $stv_result['youtube_url'] != '')
             $youtube_url = unserialize($stv_result['youtube_url']);
         else
             $youtube_url = '';
 
-            $data =[
-                "description" => $description,
-                "youtube_title" => $youtube_title,
-                "youtube_url" => $youtube_url
+        $data = [
+            "description" => $description,
+            "youtube_title" => $youtube_title,
+            "youtube_url" => $youtube_url
 
-            ];
+        ];
 
         return view('safety-training-videos')->with(compact('data'));
     }
 
-    public function firequickconductsfieldtestingofnews(){
-       
+    public function firequickconductsfieldtestingofnews()
+    {
+
 
         return view('firequick-conducts-field-testing-of-new-large-format-launcher');
     }
 
-    public function firequickproductsIncapprovesnews(){
-       
+    public function firequickproductsIncapprovesnews()
+    {
+
 
         return view('firequick-products-Inc-approves-new-large-format-flare-for-production');
     }
 
-    public function wileyxavailableatfirequickproducts(){
-       
+    public function wileyxavailableatfirequickproducts()
+    {
+
 
         return view('wiley-x-available-at-firequick-products');
     }
- 
-    public function customerService(){
+
+    public function customerService()
+    {
         $data = Post::where([
             ['post_id', 26],
-            ['type',1]
+            ['type', 1]
         ])->first();
 
         return view('customer-service')->with(compact('data'));
     }
 
-    public function privacyPolicy(){
+    public function privacyPolicy()
+    {
         $data = Post::where([
             ['post_id', 28],
-            ['type',1]
+            ['type', 1]
         ])->first();
-        
+
         return view('privacy-policy')->with(compact('data'));
     }
 
 
-    public function orderReturn(){
+    public function orderReturn()
+    {
         $data = Post::where([
             ['post_id', 32],
-            ['type',1]
+            ['type', 1]
         ])->first();
 
         return view('order-return')->with(compact('data'));
     }
-    
-    public function shippingInformation(){
+
+    public function shippingInformation()
+    {
         $data = Post::where([
             ['post_id', 33],
-            ['type',1]
+            ['type', 1]
         ])->first();
-        
+
         return view('shipping-information')->with(compact('data'));
     }
 
-    public function termAndUse(){
+    public function termAndUse()
+    {
         $data = Post::where([
             ['post_id', 34],
-            ['type',1]
+            ['type', 1]
         ])->first();
-        
+
         return view('term-use')->with(compact('data'));
     }
 
-    public function productsList($categoryId = null) {
+    public function productsList($categoryId = null)
+    {
 
         $categories = Category::all();
         $currentCategory = null;
-        if($categoryId)
-        {
-            $products = Product::where('category_id', 'LIKE', $categoryId)
-                ->orWhere('category_id', 'LIKE', $categoryId . ',%')
-                ->orWhere('category_id', 'LIKE', '%,' . $categoryId)
-                ->orWhere('category_id', 'LIKE', '%,' . $categoryId . ',%')
+        if ($categoryId) {
+            $products = Product::where('status', 1)
+                ->where(function ($query) use ($categoryId) {
+                    $query->where('category_id', 'LIKE', $categoryId)
+                        ->orWhere('category_id', 'LIKE', $categoryId . ',%')
+                        ->orWhere('category_id', 'LIKE', '%,' . $categoryId)
+                        ->orWhere('category_id', 'LIKE', '%,' . $categoryId . ',%');
+                })
                 ->get();
+
             $currentCategory = Category::where('category_id', $categoryId)->first();
+        } else {
+            $products = Product::where('status', 1)->get();
         }
-        else {
-            $products = Product::all();
-        }
-        
-            
+
+
         return view('products')->with(compact(['products', 'categories', 'currentCategory']));
     }
-    
-    public function singleProduct($productId,$productTitle) {
+
+    public function singleProduct($productId, $productTitle)
+    {
 
         $product = Product::where('product_id', $productId)->first();
         $categories = Category::all();
 
-        if(!$product)
-        {
+        if (!$product) {
             return redirect('products');
         }
 
@@ -209,11 +225,9 @@ class IndexController extends Controller
     {
         $states = State::where('country_id', $countryId)->get()->toArray();
 
-        if(count($states) <= 0)
-        {
+        if (count($states) <= 0) {
             return 'false';
-        }
-        else {
+        } else {
             return response()->json($states);
         }
     }
@@ -246,26 +260,26 @@ class IndexController extends Controller
         if (!$customer) {
             return redirect()->route('home');
         }
-    
-        
+
+
         $order = Order::where('customer_email', $customer->email)->get();
         // dd($order);
-     
-    
+
+
         $validStatuses = ['1', '2', '3', '4', '5'];
-    
+
         $orderItems = OrderItem::select('orderitems.*', 'products.title', 'orders.customer_email', 'orders.shipping_cost', 'orders.sub_total as order_subtotal', 'orders.handling_fee as total_handling_fee', 'orders.tax as total_tax')
             ->join('products', 'orderitems.product_id', '=', 'products.product_id')
             ->join('orders', 'orderitems.order_id', '=', 'orders.order_id')
             // ->where('orderitems.order_id', $orderId)
-            ->where('orders.customer_email','=', $customer->email)
+            ->where('orders.customer_email', '=', $customer->email)
             ->whereIn('orders.order_status', $validStatuses)
             ->get();
-    
-     
+
+
         // dd($orderItems);
         $user = User::select('facebook', 'instagram', 'linkedin', 'address', 'phone', 'tollfree', 'fax')->first();
-    
+
         $data = [
             'title' => 'Order Info',
             'user' => $user,
@@ -275,7 +289,7 @@ class IndexController extends Controller
         // dd($data);
         // dd($data);
         return view('myaccount.orders')->with(compact('data'));
-    }    
+    }
 
     public function sendToFriend(Request $request)
     {
@@ -299,13 +313,13 @@ class IndexController extends Controller
 
         return response()->json(['error' => 'Invalid request'], 400);
     }
-    
-    
+
+
 
 
     public function addImage()
     {
-    
+
         return view('add-image');
     }
 
@@ -314,14 +328,14 @@ class IndexController extends Controller
         // if (!Auth::check()) {
         //     return redirect()->back()->withErrors(['error' => 'You must be logged in to upload an image.']);
         // }
-  
+
 
         // Validate the form data
         $request->validate([
             'title' => 'required|string|max:55',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:5120',
         ]);
-    
+
         // Get authenticated user
         // $email = Auth::user()->email;
         $email = "ammadkhan405@gmail.com";
@@ -330,17 +344,17 @@ class IndexController extends Controller
 
 
 
-        
-    
+
+
         // Handle file upload
         if ($request->hasFile('image')) {
             $image = $request->file('image');
             $filename = time() . '_' . $image->getClientOriginalName();
-            
-           // Define the new storage path
-        $destinationPath = 'D:\laragon\www\CompleteFirequick\admin\assets\gallery';
-        $image->move($destinationPath, $filename);
-    
+
+            // Define the new storage path
+            $destinationPath = 'D:\laragon\www\CompleteFirequick\admin\assets\gallery';
+            $image->move($destinationPath, $filename);
+
             // Save image data to the database
             $data = [
                 'title' => $request->title,
@@ -350,27 +364,19 @@ class IndexController extends Controller
                 'user_id' => $customer_id,
                 'status' => 2,
             ];
-    
+
             Image::create($data);
-    
-          // Get admin email from environment variable
-  
 
-        // Send email to admin using the Mailable class
-        Mail::to(User::first()->email)->send(new ImageUploaded($email, $filename));
+            // Get admin email from environment variable
 
-    
+
+            // Send email to admin using the Mailable class
+            Mail::to(User::first()->email)->send(new ImageUploaded($email, $filename));
+
+
             return redirect()->back()->with('success', 'Image uploaded successfully.');
         } else {
             return redirect()->back()->withErrors(['image' => 'Image upload failed.']);
         }
     }
-    
-    
-    
-
-    
-    
 }
-
-
