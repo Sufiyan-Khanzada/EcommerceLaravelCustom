c07b3e6be67442269fb31b506e04df3bfb99ccb2
abdul haseeb <abdul.haseeb@quantum-computing-services.com>
2024-07-04 19:38:05 +0500

    register page validation added

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -12,62 +12,121 @@
 <section class="custom-section">
 	<div class="container">
 		<div class="row">
-			<form action="{{route('register-user')}}" method="POST"> @csrf
-				<div class="col-md-8 center no-padding">
-					<div class="col-md-12">
-						<h3>Register New Account</h3>
-						<p>Create an account by entering the information below. If you are a returning customer please login at the top of the page.</p>
-					</div>
-					<div id="error">
-						<p style="color:red;font-size:15px;font-weight:bold;"></p>
-					</div>
-					<div class="col-md-6 form-group">
-						<label class="">First Name</label>
-						<input type="text" name="fname" class="form-control input-lg" placeholder="First Name" value="" id="fname"> </div>
-					<div class="col-md-6 form-group">
-						<label class="">Last Name</label>
-						<input type="text" name="lname" class="form-control input-lg" placeholder="Last Name" value="" id="lname"> </div>
-					<div class="col-md-6 form-group">
-						<label class="">Email</label>
-						<input type="email" name="email" class="form-control input-lg" placeholder="Email" value="" id="email"> </div>
-					<div class="col-md-6 form-group">
-						<label class="">Organization / Company</label>
-						<input type="text" name="company" class="form-control input-lg" placeholder="Organization" value="" id="company"> </div>
-					<div class="col-md-6 form-group">
-						<label class="">Password</label>
-						<input type="password" name="password" class="form-control input-lg" placeholder="Password" value="" id="password"> </div>
-					<div class="col-md-12 form-group ">
-						<label class="">Address</label>
-						<input type="text" name="address1" class="form-control input-lg" placeholder="Street" value="" id="address1"> </div>
-					<div class="col-md-6 form-group">
-						<input type="text" name="address2" class="form-control input-lg" placeholder="Apartment, Unit, etc" value="" id="address2"> </div>
-					<div class="row"></div>
-					<div class="col-md-6 form-group">
-						<label class="">Country</label>
-						<select name="country" id="country" class="mycountry">
-							<option value="">select</option>
-						</select>
-					</div>
-					<div class="col-md-6 form-group">
-						<label>State</label>
-						<select name="state" id="state" style="width: 100% !important">
-							<option value="">select</option>
-						</select>
-					</div>
-					<div class="col-md-6 form-group">
-						<label>City</label>
-						<input type="text" name="city" class="form-control input-lg" placeholder="City" value="" id="city"> </div>
-					<div class="col-md-6 form-group">
-						<label class="">Postcode / Zip</label>
-						<input type="text" name="postcode" class="form-control input-lg" placeholder="Postcode / Zip" value="" id="postcode"> </div>
-					<div class="col-md-6 form-group">
-						<label class="">Phone</label>
-						<input type="text" name="phone" class="form-control input-lg" placeholder="Phone" value="" id="phone"> </div>
-					<div class="col-md-12 form-group">
-						<button type="submit" class="btn btn-default submit">Register New Account </button>
-					</div>
-				</div>
-			</form>
+		<form action="{{ route('register-user') }}" method="POST">
+    @csrf
+    <div class="col-md-8 center no-padding">
+        <div class="col-md-12">
+            <h3>Register New Account</h3>
+            <p>Create an account by entering the information below. If you are a returning customer, please login at the top of the page.</p>
+        </div>
+        
+        <div class="col-md-6 form-group">
+            <label>First Name</label>
+            <input type="text" name="fname" required class="form-control input-lg" placeholder="First Name" value="{{ old('fname') }}" id="fname">
+            @error('fname')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-6 form-group">
+            <label>Last Name</label>
+            <input type="text" name="lname" required class="form-control input-lg" placeholder="Last Name" value="{{ old('lname') }}" id="lname">
+            @error('lname')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-6 form-group">
+            <label>Email</label>
+            <input type="email" name="email" required class="form-control input-lg" placeholder="Email" value="{{ old('email') }}" id="email">
+            @error('email')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-6 form-group">
+            <label>Organization / Company</label>
+            <input type="text" name="company" required class="form-control input-lg" placeholder="Organization" value="{{ old('company') }}" id="company">
+            @error('company')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-6 form-group">
+            <label>Password</label>
+            <input type="password" name="password" required class="form-control input-lg" placeholder="Password" id="password">
+            @error('password')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-12 form-group">
+            <label>Address</label>
+            <input type="text" name="address1" required class="form-control input-lg" placeholder="Street" value="{{ old('address1') }}" id="address1">
+            @error('address1')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-6 form-group">
+            <input type="text" name="address2" required class="form-control input-lg" placeholder="Apartment, Unit, etc" value="{{ old('address2') }}" id="address2">
+            @error('address2')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-6 form-group">
+            <label>Country</label>
+            <select name="country" id="country" required class="mycountry">
+                <option value="">select</option>
+                <!-- Options should be populated here -->
+            </select>
+            @error('country')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-6 form-group">
+            <label>State</label>
+            <select name="state" id="state"  style="width: 100% !important">
+                <option value="">select</option>
+                <!-- Options should be populated here -->
+            </select>
+            @error('state')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-6 form-group">
+            <label>City</label>
+            <input type="text" name="city" class="form-control input-lg" placeholder="City" value="{{ old('city') }}" id="city">
+            @error('city')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-6 form-group">
+            <label>Postcode / Zip</label>
+            <input type="text" name="postcode" required class="form-control input-lg" placeholder="Postcode / Zip" value="{{ old('postcode') }}" id="postcode">
+            @error('postcode')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-6 form-group">
+            <label>Phone</label>
+            <input type="text" name="phone" required class="form-control input-lg" placeholder="Phone" value="{{ old('phone') }}" id="phone">
+            @error('phone')
+                <span class="text-danger">{{ $message }}</span>
+            @enderror
+        </div>
+        
+        <div class="col-md-12 form-group">
+            <button type="submit" class="btn btn-default submit">Register New Account</button>
+        </div>
+    </div>
+</form>
+
 		</div>
 	</div>
 </section>
